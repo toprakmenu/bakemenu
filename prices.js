cf35657c39ec99eab7ee1c89cbff17c474b27740
toprakmenu <fatihkaraman.41.fk@gmail.com>
2026-07-14 15:01:11 +0300

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,3 +1,27 @@
 const prices = {
 
+    // Kahvaltı
+
+    // Tartineler
+
+    // Kruvasan Sandviçler
+
+    // Focaccia Sandviçler
+
+    // Kruvasanlar
+
+    // Poğaçalar
+
+    // Tatlılar
+
+    // Sıcak İçecekler
+
+    // Soğuk İçecekler
+
+    // Ekmekler
+
+    // Tezgah Ürünleri
+
+    // Sporcu Menüsü
+
 };
