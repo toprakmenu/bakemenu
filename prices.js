4856f7c5618b2b366552a1930690983f30c45365
toprakmenu <fatihkaraman.41.fk@gmail.com>
2026-07-14 15:31:27 +0300

    Update prices.js

--- prices.js
+++ prices.js
@@ -21,6 +21,15 @@ focacciaHindiFume: 440,
 focacciaRozbif: 460,
 
     // Kruvasanlar
+sadeKruvasan: 180,
+patlicanliDanish: 250,
+cikolataliKruvasan: 200,
+bademliKruvasan: 380,
+ormanMeyveliDanish: 230,
+pastaciKremaliCilekliKruvasan: 400,
+findikEzmeliMuzluKruvasan: 400,
+antepFistikliKruvasan: 410,
+antepFistikliVisneliKruvasan: 430,
 
     // Poğaçalar
 
